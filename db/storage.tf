c3766e2a4ced821b8f31ed99bcd8eec4d664d329
daphney <bot.skillment@gmail.com>
2021-11-04 16:35:20 +0100

    edited the cloud storage block

--- db/storage.tf
+++ db/storage.tf
@@ -6,7 +6,13 @@ resource "google_storage_bucket_object" "db_instance" {
 
 resource "google_storage_bucket_object" "db_user" {
   name   = "db_user"
-  content = "${google_sql_user.admin_user}"
+  content = "${google_sql_user.admin_user.name}"
+  bucket = "sca-project"
+}
+
+resource "google_storage_bucket_object" "db_pass" {
+  name   = "db_pass"
+  content = "${google_sql_user.admin_user.password}"
   bucket = "sca-project"
 }
 
