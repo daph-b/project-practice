5142d311f7a84f9ce3318e99ed73a46fbb3fcfc5
daph-b <92843539+daph-b@users.noreply.github.com>
2021-11-04 14:51:53 +0100

    Update data.tf
    replaced the reference to the terraform remote state data source  with variables

--- kubernetes/data.tf
+++ kubernetes/data.tf
@@ -6,13 +6,13 @@ data "google_client_config" "default" {}
 
 // get information about cluster
 data "google_container_cluster" "sca_cluster" {
-  name = data.terraform_remote_state.sca-cluster.outputs.cluster_name
-  location = data.terraform_remote_state.sca-cluster.outputs.cluster_zone
+  name = var.cluster_name
+  location = var.cluster_zone
 }
 
 
 //get data about the cloud sql instance we want to connect to
 data "google_sql_database_instance" "app_db" {
-    name = data.terraform_remote_state.sca-database.outputs.db_instance
+    name = var.db_instance
 } 
 
